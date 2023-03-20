e8a04a6624fe90c4c83678d092cf4f7eeb58fcdb
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-03-21 00:09:08 +0900

    Fix

--- parseDate.ts
+++ parseDate.ts
@@ -1,9 +1,7 @@
-function parseDate(dateString: string): Date {
+export function parseDate(dateString: string): Date {
   const date = new Date(dateString);
   if (isNaN(date.getTime())) {
     throw new Error('Invalid date string');
   }
   return date;
 }
-
-export default parseDate;
