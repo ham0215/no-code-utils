db1f8bb4cb0244abf32cbf8d53abfeb58f537dd2
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-03-28 23:46:53 +0900

    Remove unused file

--- parseDate.ts
+++
@@ -1,7 +0,0 @@
-export function parseDate(dateString: string): Date {
-  const date = new Date(dateString);
-  if (isNaN(date.getTime())) {
-    throw new Error('Invalid date string');
-  }
-  return date;
-}
