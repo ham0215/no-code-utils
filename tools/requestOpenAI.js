9e4f27875d9d5b1ba1a98ef435294ae1f77374dd
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-03-29 00:56:37 +0900

    Fix requestOpenAi

--- tools/requestOpenAI.js
+++ tools/requestOpenAI.js
@@ -1,29 +1,59 @@
-const { Configuration, OpenAIApi } = require("openai");
 const fs = require('fs');
+const path = require('path');
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+async function requestOpenAI(funcName, design) {
+  const { Configuration, OpenAIApi } = require("openai");
 
-async function requestOpenAI(openai) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const content = `
+    次の要件を満たすTypescriptの関数を返却してください。
+    コードの説明や前置きは不要です。コードだけ返却してください。
+    関数にはexportをつけてください。
+    関数名は${funcName}。
+    ${design}
+  `;
+
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages: [
-      {"role": "user", "content": "次の要件を満たすTypescriptの関数を返却してください。コードだけ返却してください。・関数名はparseDate・文字列をDate型に変換する・変換に失敗した場合はundefinedを返却する"}
+      { role: 'user', content }
     ]
   });
 
-  console.log(completion.data.choices[0].message.content);
+  if (completion.status !== 200) throw new Error(completion.response.data.error.message);
+
+  saveFile(funcName, completion.data.choices[0].message.content);
+  // rate limit
+  await sleep(3500);
 };
 
-// requestOpenAI(openai);
+async function getFiles() {
+  const files = fs.readdirSync('./docs');
 
-const res = "```typescript\nfunction parseDate(dateString: string): Date | undefined {\n  const date = new Date(dateString);\n  return isNaN(date.getTime()) ? undefined : date;\n}\n```";
-let result = res;
-const patterns = [/```typescript/g, /```/g];
-patterns.forEach(pattern => {
-  result = result.replace(pattern, '');
-});
+  for (let i = 0; i < files.length; i++) {
+    await readFile(files[i]);
+  }
+};
 
-console.log(result);
+async function readFile(fileName) {
+  console.info(fileName);
+  const data = fs.readFileSync(path.join('./docs', fileName), 'utf-8');
+  console.info(data);
+  await requestOpenAI(fileName.replace(/\.txt$/g, ''), data);
+};
+
+async function saveFile(fileName, data) {
+  await fs.promises.writeFile(`./${fileName}.ts`, data);
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+getFiles();
